81c05d2387edca4e0646e25e144433b508e85ff9
weijie.cao <15757115966@163.com>
2018-03-15 22:21:55 +0800

    1

--- src/app/Add.js
+++ src/app/Add.js
@@ -54,35 +54,37 @@ class Add extends Component {
   render() {
     return (
       <div className="height100">
-        <div className="add-title">
-          <i className="iconfont">&#xe626;</i>
-          <div className="form-inline form-control-sm">
-            <input
-              className="form-control"
-              type="datetime"
-              name="time"
-              id="time1"
-              value={this.state.time}
-              onChange={this.handleChange.bind(this, 'time')}
-            />
-          </div>
-        </div>
-        <div className="price">
-          <div className="input-group input-group-lg">
-            <div className="input-group-prepend">
-              <span className="input-group-text">
-                <i className="iconfont">&#xe600;</i>
-              </span>
+        <div className="top-ctn">
+          <div className="add-title">
+            <i className="iconfont">&#xe626;</i>
+            <div className="form-inline form-control-sm">
+              <input
+                className="form-control"
+                type="datetime"
+                name="time"
+                id="time1"
+                value={this.state.time}
+                onChange={this.handleChange.bind(this, 'time')}
+              />
             </div>
-            <input
-              className="form-control"
-              type="number"
-              value={this.state.price}
-              onChange={this.handleChange.bind(this, 'price')}
-            />
           </div>
+          <div className="price">
+            <div className="input-group input-group-lg">
+              <div className="input-group-prepend">
+                <span className="input-group-text">
+                  <i className="iconfont">&#xe600;</i>
+                </span>
+              </div>
+              <input
+                className="form-control"
+                type="number"
+                value={this.state.price}
+                onChange={this.handleChange.bind(this, 'price')}
+              />
+            </div>
+          </div>
+          <Categories defaultId={this.state.categoryId} clickFunc={this.categoryIdChange.bind(this)} />
         </div>
-        <Categories defaultId={this.state.categoryId} clickFunc={this.categoryIdChange.bind(this)} />
         <div className="calculator">
           <div className="price-ctn">
             <div className="processing">{this.state.input}</div>
